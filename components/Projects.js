5b9aa0b132b0a17543a024382ee4a08c1f3e813e
Youssef Durgham <73782996+Youssef-Durgham@users.noreply.github.com>
2023-05-05 00:47:55 +0300

    Update Projects.js

--- components/Projects.js
+++ components/Projects.js
@@ -47,6 +47,54 @@ const Projects = () => {
 
 
                 <div className="mt-20 t:mt-40">
+                  <Link href="https://master.d1qws3vy9tc2wb.amplifyapp.com/admin-one-react-tailwind/dashboard">
+      <Image src={"/Image/Screenshot 2023-05-05 at 12.38.26 AM.png"} width={700} height={500} className="rounded-3xl cursor-pointer"></Image>
+                </Link>
+      </div>
+      <div className='grid  place-items-center mb-20  mt-5 t:mt-5 t:text-2xl font-bold text-xl l:text-3xl'>Admin Taxi Dashboard
+                <div className='mt-10 font-normal'>an full-stack admin dashboard for a taxi mobile app the phone number for sign in is 0000 and password is 0000.</div>
+                <div className='font-normal mt-5'>build using:</div>
+                <div className=" grid grid-cols-2 t:grid-cols-4 gap-2 place-items-center grid-flow-row-dense font-normal justify-between">
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">Node.js</div>
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">Express.js</div>
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">js</div>
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">Mongodb</div>
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">React.js</div>
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">Next.js</div>
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">Tailwindcss</div>
+                
+                </div>
+                
+                <Link href="https://master.d1qws3vy9tc2wb.amplifyapp.com/admin-one-react-tailwind/dashboard">
+                <a  className="mt-10"><button type="button" className=" px-5 py-3 font-semibold rounded-2xl bg-[#14243c] text-white">Live View</button></a>
+                </Link>
+                
+                
+                </div>
+
+                <div className="mt-20 t:mt-40">
+                 
+      <Image src={"/Image/photo_2023-05-05 00.44.36.jpeg"} width={700} height={500} className="rounded-3xl"></Image>
+                
+      </div>
+      <div className='grid  place-items-center mb-20  mt-5 t:mt-5 t:text-2xl font-bold text-xl l:text-3xl'>unofficial world islamic bank mobile app
+                <div className='mt-10 font-normal'>a mobile app for bank.</div>
+                <div className='font-normal mt-5'>build using:</div>
+                <div className=" grid grid-cols-2 t:grid-cols-4 gap-2 place-items-center grid-flow-row-dense font-normal justify-between">
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">Node.js</div>
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">Express.js</div>
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">js</div>
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">Mongodb</div>
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">react native</div>
+                <div className="d-inline-flex p-1 bg-[#95D5ED] w-fit mt-5">Tailwindcss</div>
+                
+                </div>
+                
+                
+                
+                </div>
+
+                <div className="mt-20 t:mt-40">
                   <Link href="https://t.me/AY_ROCKET_TRACKER">
       <Image src={"/Image/telegrambot.jpg"} width={700} height={500} className="rounded-3xl cursor-pointer"></Image>
                 </Link>
